4b1d7f8c7b09f871c64e9200b3c95ff834fb34e6
cyanhere <90555087+cyanhere@users.noreply.github.com>
2024-07-16 14:27:22 +0800

    Create lockbutton.cpp

---
+++ Sesto_Lock/lockbutton.cpp
@@ -0,0 +1,55 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
+#include "ArduinoJson.h"
+#include <Arduino_JSON.h>
+
+const char* ssid = "TP-Link_DE83";
+const char* password = "46193346";
+
+const char* serverName = "http://192.168.0.104/request";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Server started");
+
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, serverName);
+  http.addHeader("Content-Type", "application/json");
+
+  // Create JSON content
+  DynamicJsonDocument doc(1024);
+  doc["action"] = "open";
+  String jsonString;
+  serializeJson(doc, jsonString);
+
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(jsonString);
+
+  // Print the response
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println(httpResponseCode);   // HTTP response code
+    Serial.println(response);           // HTTP response
+  } else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+  }
+
+
+  int httpResponseCode = http.POST();
+  http.end();
+}
